7a9362d67ef3ea991b6e3e8a69311ce2bfc96ebb
kitty <141717449+kitty-crow@users.noreply.github.com>
2026-08-05 11:36:30 +0100

    Restore converter operation mappings

---
+++ tfjs-converter/src/operations/op_list/basic_math.ts
@@ -0,0 +1,4 @@
+/** @license Copyright 2022 Google LLC. Licensed under Apache-2.0. */
+import data from './basic_math.json';
+import type {OpMapper} from '../types';
+export const json = data as OpMapper[];
---
+++ tfjs-converter/src/operations/op_list/string.ts
@@ -0,0 +1,4 @@
+/** @license Copyright 2022 Google LLC. Licensed under Apache-2.0. */
+import data from './string.json';
+import type {OpMapper} from '../types';
+export const json = data as OpMapper[];
