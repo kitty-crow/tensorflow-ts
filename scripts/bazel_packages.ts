8d6d1cd7d1025a6c2f8a0a48bce5ca8322886a3b
kitty <141717449+kitty-crow@users.noreply.github.com>
2026-08-05 09:47:17 +0100

    Focus TensorFlow.js package set

--- scripts/bazel_packages.ts
+++ scripts/bazel_packages.ts
@@ -1,12 +1,9 @@
 export const BAZEL_PACKAGES = new Set([
   'tfjs-core',
   'tfjs-backend-cpu',
-  'tfjs-tfdf',
-  'tfjs-tflite',
   'tfjs-converter',
   'tfjs-backend-webgl',
   'tfjs-backend-webgpu',
   'tfjs-layers',
   'tfjs-data',
-  'tfjs-backend-wasm',
 ]);
